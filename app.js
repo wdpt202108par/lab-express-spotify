18857024388de08671e54d0e8db7b03b031db4ef
Yooyoo56 <tak.yoojeong@gmail.com>
2021-11-10 11:16:01 +0100

    iteration 3 almost done

--- app.js
+++ app.js
@@ -23,11 +23,28 @@ spotifyApi
     .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 // Our routes go here:
-
+//homepage
 app.get('/', (req, res)=> {
-  //  res.send('<h1>Hello welcome to spotify</h1>');
   res.render('index');
-    console.log('Website is launching..');
+//console.log('Website is launching .. 👾');
 })
+
+
+//artist search page
+  app.get('/artist-search', (req, res)=> {
+
+    spotifyApi
+    .searchArtists(req.query.artists)
+    .then(data => {
+     // console.log('The received data from the API: ', data.body);
+    // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+      res.render('artist-search-results',data.body.artists);
+    })
+    .catch(err => console.log('The error while searching artists occurred: ', err));
+  })
+
+  //albums
+
+
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
